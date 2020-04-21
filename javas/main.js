f92b38ff44b772971251ca799d9c15953402ba03
Igor <igorroot@outlook.com.br>
2020-04-21 19:51:25 -0300

    adicionei prompt

--- javas/main.js
+++ javas/main.js
@@ -2,3 +2,11 @@ var nome="IGOR";
 alert(" YNGVARR AREA")
 alert("ENTRADA AUTORIZADA APENAS PRA PESSOAS SELECIONADAS")
 alert("CLIQUE OK APENAS SE TIVER SIDO DIRECIONADO POR " + nome)
+
+var nome = prompt("Fala o nome ai '-'");
+if (nome= ester , Ester , Ketlen , ketlen , Bruno , bruno , Cilie , cilie , Felipe , felipe){
+    alert("VC PDOE ACESSAR A PAGE =)");
+}
+else{
+    alert("FECHE A PAGE AGR, CASO NAO FIZER EU VOU SABER >:(");
+}
